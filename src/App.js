0ea967e5f1b34488dc758fc1fb309d255ab28aa8
costanzopa <costanzopa@gmail.com>
2019-10-13 02:50:49 -0300

    Begining with routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return (
+const HatsPage = () => (
     <div>
-      <HomePage />
+        <h1>HATS PAGE </h1>
     </div>
-  );
+);
+
+
+function App() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/hats' component={HatsPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,51 +5,70 @@ import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 class Directory extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5
-        }
-      ]
-    };
-  }
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'hats'
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: ''
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3,
+                    linkUrl: ''
+                },
+                {
+                    title: 'womens',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: ''
+                },
+                {
+                    title: 'mens',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: ''
+                },
+                {
+                    title: 'ungendered',
+                    imageUrl: 'https://images.squarespace-cdn.com/content/v1/57b1c6fdbe6594dc4a678df1/1551378076820-TJBUCCT9RC5BQC123F7S/ke17ZwdGBToddI8pDm48kFWxnDtCdRm2WA9rXcwtIYR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcTSrQkGwCGRqSxozz07hWZrYGYYH8sg4qn8Lpf9k1pYMHPsat2_S1jaQY3SwdyaXg/gn3.jpg?format=1500w',
+                    id: 6,
+                    size: 'large',
+                    linkUrl: ''
+                },
+                {
+                    title: 'kids',
+                    imageUrl: 'https://peopledotcom.files.wordpress.com/2018/11/celine-dion-5.jpg?w=2606&h=1738',
+                    id: 7,
+                    size: "large",
+                    linkUrl: ''
+                }
+            ]
+        };
+    }
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        ))}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps} />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Directory;
