733acc79af295d8c92655cb4cf7e5d8f65a1f588
costanzopa <costanzopa@gmail.com>
2019-10-14 01:59:23 -0300

    Adding sign in page.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
+import SignInPage from './pages/signin/sign-in-page.component';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInPage}/>
             </Switch>
         </div>
     );
---
+++ src/pages/signin/sign-in-page.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './sign-in-page.styles.scss';
+
+const SignInPage = () => (
+    <div className='sign-in'>
+        Sign In
+    </div>
+);
+
+export default SignInPage;
