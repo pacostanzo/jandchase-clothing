72358ecf08cda38bb60963740b26f38769e71b23
costanzopa <costanzopa@gmail.com>
2019-10-14 17:27:57 -0300

    Adding style to the sign in page.

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-buttom/custom-buttom.component";
+
+class SignIn extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''})
+    };
+
+    handleChange = (event) => {
+        const {value, name} = event.target;
+
+
+        this.setState({[name]: value})
+    };
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2 className='title'>I already have an account</h2>
+                <span className='subtitle'>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email"
+                               name="email"
+                               label='email'
+                               value={this.state.email}
+                               handleChange={this.handleChange}
+                               required={true}/>
+                    <FormInput type="password"
+                               name="password"
+                               label='password'
+                               value={this.state.password}
+                               handleChange={this.handleChange}
+                               required={true}/>
+                    <CustomButton type='submit'> Sign in </CustomButton>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default SignIn;
