8739525f5ece0e1853e9252362e591c5c7589e1e
costanzopa <costanzopa@gmail.com>
2019-11-06 16:27:27 -0300

    Setting up git components.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from "react-redux";
 
 import {ReactComponent as Logo} from '../../assets/needle.svg';
 
@@ -31,4 +32,9 @@ const Header = ({currentUser}) => (
     </div>
 );
 
-export default Header;
+// state is the root reducer which have a user: userReducer
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
