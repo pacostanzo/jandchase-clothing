5301702e0521b489d3877e27c64a785e4fbe4061
costanzopa <costanzopa@gmail.com>
2019-11-06 17:56:43 -0300

    Adding cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import './header.styles.scss';
 
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from "../cart/cart-icon/cart-icon.component";
+import CartDropdown from "../cart/cart-dropdown/cart-dropdown.component";
 
 const Header = ({currentUser}) => (
     <div className='header'>
@@ -31,6 +32,7 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 );
 
