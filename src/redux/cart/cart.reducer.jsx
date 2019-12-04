59b27176106daa0b32faeca9d8442669c93f417e
costanzopa <costanzopa@gmail.com>
2019-12-04 15:33:18 -0300

    Adding cart items.

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -12,10 +13,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             };
-        case (CartActionTypes.ADD_ITEM):
+        case CartActionTypes.ADD_ITEM:
             return {
-              ...state,
-                cartItems: [...state.cartItems, action.payload]
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             };
         default:
             return state;
