577f509101c0fbc9a15d24b37944aee9bb4ed784
costanzopa <costanzopa@gmail.com>
2020-04-24 00:42:26 -0300

    Hidding cart dropdown on click outside.

--- client/src/components/cart/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart/cart-dropdown/cart-dropdown.component.jsx
@@ -1,40 +1,67 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {connect} from "react-redux";
 import {createStructuredSelector} from 'reselect';
 
 import CustomButton from '../../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import {selectCartItems} from '../../../redux/cart/cart.selectors';
+import {selectCartItems, selectCartHidden} from '../../../redux/cart/cart.selectors';
 
 import {toggleCartHidden} from "../../../redux/cart/cart.actions";
 
 import './cart-dropdown.styles.scss';
 import {withRouter} from "react-router-dom";
 
-const CartDropdown = ({cartItems, history, dispatch}) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            {
-                cartItems.length ?
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} item={cartItem}/>
-                    ))
-                    : (<span className='empty-message'>Your Cart is Empty</span>)
-            }
-        </div>
-        <CustomButton onClick={()=>
-            {
+const CartDropdown = ({cartItems, hidden, history, dispatch}) => {
+    const node = useRef();
+
+    const handleClickOutside = e => {
+        console.log("clicking anywhere");
+        if (node.current.contains(e.target)) {
+            // inside click
+            return;
+        }
+        // outside click
+        dispatch(toggleCartHidden());
+    };
+
+    useEffect(() => {
+        if (!hidden) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [hidden]);
+
+    return (
+        <div ref={node} className='cart-dropdown'>
+            <div className='cart-items'>
+                {
+                    cartItems.length ?
+                        cartItems.map(cartItem => (
+                            <CartItem key={cartItem.id} item={cartItem}/>
+                        ))
+                        : (<span className='empty-message'>Your Cart is Empty</span>)
+                }
+            </div>
+            <CustomButton onClick={() => {
                 history.push('/checkout');
                 dispatch(toggleCartHidden());
             }
-        }>
-            GO TO CHECKOUT
-        </CustomButton>
-    </div>
-);
+            }>
+                GO TO CHECKOUT
+            </CustomButton>
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems
+    cartItems: selectCartItems,
+    hidden: selectCartHidden
 });
+
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
