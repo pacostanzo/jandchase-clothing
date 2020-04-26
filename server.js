4a35d9fac18bade9a536645d5d761c53119ef9c7
costanzopa <costanzopa@gmail.com>
2020-04-26 17:44:29 -0300

    Fixing PWA

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -14,13 +13,11 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(compression);
-    app.use(enforce.HTTPS({trustProtoHeader: true}));
-    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
