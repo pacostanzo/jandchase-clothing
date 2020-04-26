84ab3ce1bafd1256145d54013421877daec62352
costanzopa <costanzopa@gmail.com>
2020-04-26 17:58:19 -0300

    Adding compression.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -13,10 +14,11 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression);
+    app.use(enforce.HTTPS({trustProtoHeader: true}));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
@@ -26,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
     if (error) throw error;
-    console.log('Server running on port ' + port);
+    console.log('Server is running on port ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
